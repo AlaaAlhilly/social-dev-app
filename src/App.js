0734c2b9a31a40893880afb46b24ea2165778d4f
Alaa_Naji <Alhilly@AlaaMac.local>
2019-03-21 14:15:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/layout/Navbar'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import NewSnippet from './components/pages/NewSnippet'
+import Profile from './components/pages/Profile'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="root">
+          <Navbar />
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/new-snippet" exact={true} component={NewSnippet}/>
+          <Route path="/profile" exact={true} component={Profile}/>
+          <Route path="/snippets" exact={true} component={Home}/>
+          
+        </div>
+      </Router>
     );
   }
 }
